073ae43a24c747b839728ea3cf3bac4a73c852da
Rikuto <mikadorikuto@gmail.com>
2026-03-12 14:51:37 +0900

    code updated

---
+++ backend/api/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+# Create your models here.
+class NewsArticle(models.Model):
+    title = models.CharField(max_length=250)
+    content = models.TextField(blank=True, null=True)
+    url = models.URLField(unique=True)
+    source_name = models.CharField(max_length=100)
+    author = models.CharField(max_length=250, blank=True, null=True)
+    published_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["-published_date"]
